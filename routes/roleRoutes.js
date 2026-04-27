da6ff2bea02f152076b0dedeaf8a056c508f9419
rfqwebapp-creator <rfqwebapp@gmail.com>
2026-04-27 20:13:17 +0530

    first commit

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const roleController = require("../controllers/roleController");
-
 router.post("/create", roleController.createRole);
 router.get("/all", roleController.getAllRoles);
-router.get("/:id", roleController.getRoleById);
+
+// 🔥 DELETE FIRST
 router.delete("/:id", roleController.deleteRole);
 
+// THEN GET BY ID
+router.get("/:id", roleController.getRoleById);
 module.exports = router;
